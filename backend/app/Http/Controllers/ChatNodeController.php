a4fdc7722fdd71ad66128343a0a7719aedb1f99c
Dir-Bien <107493215+Dir-Bien@users.noreply.github.com>
2025-12-03 17:41:00 -0300

    crud operation

--- backend/app/Http/Controllers/ChatNodeController.php
+++ backend/app/Http/Controllers/ChatNodeController.php
@@ -3,10 +3,95 @@
 namespace App\Http\Controllers;
 
 use App\Models\ChatNode;
+use Exception;
 use Illuminate\Http\Request;
 
 class ChatNodeController extends Controller
 {
+    public function get_all_nodes() {
+        return ChatNode::all();
+    }
+
+    public function get_node_by_id($id) {
+        return ChatNode::find($id);
+    }
+
+    public function insert(Request $request) {
+        try{
+            ChatNode::create([
+                'question' => $request->question,
+                'options' =>  $request->input('options'),
+                'next_nodes' => $request->input('next_nodes'),
+                'is_end' => $request->is_end
+            ]);
+
+            $response = [
+                'message' => 'El mensaje se creo correctamente.',
+                'status' => 201
+            ];
+
+            return response()->json($response);
+        } catch(Exception $e) {
+            $response = [
+                'message' => 'Ocurrio un error, revise los campos o hablen con desarrollo XD',
+                'error' => $e,
+                'status' => 500
+            ];
+
+            return response()->json($response);
+        }
+    }
+
+    public function update($id, Request $request){
+        try {
+            $node = ChatNode::find($id);
+
+            $node->question = $request->question;
+            $node->options = $request->input('options');
+            $node->next_nodes = $request->input('next_nodes');
+            $node->is_end = $request->is_end;
+
+            $node->save();
+
+            $response = [
+                'message' => 'El mensaje se actualizo correctamente. Bien peke',
+                'status' => 500
+            ];
+
+            return response()->json($response);
+        } catch (Exception $e) {
+            $response = [
+                'message' => 'Ocurrio un error, revise los campos o hablen con desarrollo XD',
+                'error' => $e,
+                'status' => 500
+            ];
+
+            return response()->json($response);
+        }
+    }
+
+    public function destroy($id) {
+        try {
+            $node = ChatNode::find($id);
+            $node::delete();
+
+            $response = [
+                'message' => 'El mensaje se elimino correctamente',
+                'status' => 201
+            ];
+
+            return response()->json($response);
+        } catch (Exception $e) {
+            $response = [
+                'message' => 'Ocurrio un error, revise los campos o hablen con desarrollo XD',
+                'error' => $e,
+                'status' => 500
+            ];
+
+            return response()->json($response);
+        }
+    }
+
     public function start() {
         return ChatNode::find(1);
     }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/chat/start', [ChatNodeController::class, 'start']);
-
 Route::post('/chat/next', [ChatNodeController::class, 'next']);
 
+Route::get('/nodes/all', [ChatNodeController::class, 'get_all_nodes']);
+Route::get('/nodes/{id}', [ChatNodeController::class, 'get_node_by_id']);
+
+Route::post('/nodes/create', [ChatNodeController::class, 'insert']);
+Route::put('/nodes/update/{id}', [ChatNodeController::class, 'update']);
+Route::delete('/nodes/delete', [ChatNodeController::class, 'destroy']);
