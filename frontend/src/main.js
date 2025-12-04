508713190da251fd094f6e7490879d4e1f865e29
Dir-Bien <107493215+Dir-Bien@users.noreply.github.com>
2025-12-04 17:36:31 -0300

    add vuesax

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import Vuesax from 'vuesax-alpha'
+import 'vuesax-alpha/dist/index.css'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
 
 createApp(App)
     .use(router)
+    .use(Vuesax)
     .mount('#app')
--- frontend/src/pages/CreatePage.vue
+++ frontend/src/pages/CreatePage.vue
@@ -1,19 +1,20 @@
+<script>
+export default {
+      data:() => ({
+        value: ''
+      })
+    }</script>
+
 <template>
     <div class="container">
         <h1>Create Message</h1>
         <form>
-        <div class="mb-3">
-            <input class="form-control" id="exampleInputEmail1">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Mensaje*</label>
-            <input class="form-control" id="exampleInputEmail1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="center content-inputs">
+                <vs-input
+                    label-placeholder="Country"
+                    v-model="value"/>
+                </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
     
